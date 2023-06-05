ffad325b817bdfa994ca5dc06424be204a5e03b9
samane <zahra.mahjour@gmail.com>
2023-06-05 14:39:59 +0330

    make files

---
+++ models.py
@@ -0,0 +1,40 @@
+
+class Contact:
+
+    def __init__(self, name, email, phone):
+        self.name = name
+        self.email = email
+        self.phone = phone
+
+    
+    def add_contact(self, cls):
+        pass
+
+
+    def edit_contact(self):
+        pass
+
+
+    def delete_contact(self):
+        pass
+
+
+
+class User:
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+
+    def create_account(self):
+        pass
+
+
+    def authenticate_account(self):
+        pass
+
+
+    def modify_account(self):
+        pass
+
