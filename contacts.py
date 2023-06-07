b925cf6b90d966f8827c4d4ad1643a430e5e71a0
samane <zahra.mahjour@gmail.com>
2023-06-07 14:09:05 +0330

    write a number of methods in contact class

--- contacts.py
+++ contacts.py
@@ -18,27 +18,43 @@ class Contact:
         cls.contacts.append(new_contact)
 
 
-    @classmethod
+    @staticmethod
+    def get_list_of_all():
+        with open("contacts.pickle", "rb") as f:
+            pickle.load(f)
+            return f
+
+
     def edit_contact(self):
         pass
 
-
-    def delete_contact(self):
-        pass
-
+    
     @staticmethod
-    def save_contact():
+    def save_contact_to_file():
         with open("contacts.pickle", "wb") as f:
             pickle.dump(Contact.contacts, f)
 
 
-    def search_contact_by_name(self):
-        pass
+    @classmethod
+    def find_contact_by_name(cls, name):
+        contacts_lst = cls.get_list_of_all()
+        for contact in contacts_lst:
+            if contact.name == name:
+                return contact
 
 
-    def search_contact_by_email(self):
-        pass
+    @classmethod
+    def find_contact_by_email(cls, email):
+        contacts_lst = cls.get_list_of_all()
+        for contact in contacts_lst:
+            if contact.email == email:
+                return contact
 
 
     def group_contacts_into_catogories(self):
         pass
+
+
+    @classmethod
+    def delete_contact(cls, name):
+        del cls.find_contact_by_name(name)
